4a0924bc03df461eb48f78962f085df75979eb7c
Chheang.chhea <chheangchhea008@gmail.com>
2023-06-09 09:30:24 +0700

    test

---
+++ src/app/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CardLogin() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      <h1>Hello</h1>
+      
+    </main>
+  )
+}
